8e422ce6b189b2c3085363b18e195b79d3289040
pdwebApp <67463874+pdwebApp@users.noreply.github.com>
2026-03-11 14:41:32 +0000

    Update dark mode charts and UI improvements

--- app.py
+++ app.py
@@ -290,8 +290,8 @@ def goal():
 # RUN APP
 # =====================================================
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
 # if __name__ == "__main__":
-#     port = int(os.environ.get("PORT", 8080))
-#     app.run(host="0.0.0.0", port=port)
+#     app.run(debug=True, host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
